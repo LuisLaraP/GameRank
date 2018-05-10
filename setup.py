e210d1481ac855dfa1206427675c57f3f6ad3b76
Luis Lara <luislpatino@gmail.com>
2018-05-09 19:54:16 -0500

    Create setup file

---
+++ setup.py
@@ -0,0 +1,15 @@
+"""Setup script."""
+
+from setuptools import find_packages, setup
+
+
+setup(
+	name='GameRank',
+	description='A video game ranking system powered by machine learning.',
+	version='0.0.0',
+	author='Luis Alejandro Lara Patiño',
+	author_email='luislpatino@gmail.com',
+	license='GPLv3',
+	packages=find_packages(),
+	python_requires='>=3'
+)
