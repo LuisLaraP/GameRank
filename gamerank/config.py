00c57fe09119b1e6e2eb5bc1c989e3198132750c
Luis Lara <luislpatino@gmail.com>
2018-05-22 10:23:38 -0500

    Read configuration file

--- gamerank/config.py
+++ gamerank/config.py
@@ -1,5 +1,7 @@
 """Configuration loading and saving."""
 
+import configparser
+
 import appdirs
 
 
@@ -9,3 +11,11 @@ appName = 'GameRank'
 def databasePath():
 	"""Return the absolute path to the root directory of the database."""
 	return appdirs.user_data_dir(appName) + '/database'
+
+
+def readConfig():
+	"""Read the configuration file and return the parsed contents."""
+	path = appdirs.user_config_dir(appName) + '/GameRank.cfg'
+	config = configparser.ConfigParser()
+	config.read(path)
+	return config
