feb8b3fc27cb69adce375c156bb1112d4ae67ef5
Luis Lara <luislpatino@gmail.com>
2018-05-29 20:57:54 -0500

    Graph validation curve

--- gamerank/parameters.py
+++ gamerank/parameters.py
@@ -28,3 +28,27 @@ def learningRate():
 		plt.semilogy(range(1, errorCurve.size + 1), errorCurve, label=str(lr))
 	plt.legend()
 	plt.show()
+
+
+def regularization():
+	"""Plot error vs iterations."""
+	alphaList = [0.0001, 0.001, 0.01, 0.1, 1, 10, 100, 1000]
+	xTrain = db.load('train', 'data')[:, 1:].astype(int)
+	yTrain = np.squeeze(db.load('train', 'y')[:, 1:])
+	xValid = db.load('valid', 'data')[:, 1:].astype(int)
+	yValid = np.squeeze(db.load('valid', 'y')[:, 1:])
+	eTrain = np.zeros(len(alphaList))
+	eValid = np.zeros(len(alphaList))
+	for i in range(len(alphaList)):
+		for _ in range(5):
+			model = SGDRegressor(penalty='l2', learning_rate='constant', eta0=0.006,
+				max_iter=100)
+			model.fit(xTrain, yTrain)
+			eTrain[i] += np.sqrt(mean_squared_error(yTrain, model.predict(xTrain)))
+			eValid[i] += np.sqrt(mean_squared_error(yValid, model.predict(xValid)))
+		eTrain[i] /= 5
+		eValid[i] /= 5
+	plt.semilogx(alphaList, eTrain, label='Training')
+	plt.semilogx(alphaList, eValid, label='Validation')
+	plt.legend()
+	plt.show()
