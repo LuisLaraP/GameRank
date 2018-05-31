49c5b28d0827bfeba4b24c86c3e39ec2b96349ac
Luis Lara <luislpatino@gmail.com>
2018-05-31 10:28:08 -0500

    Perform regression on summaries

--- gamerank/regression.py
+++ gamerank/regression.py
@@ -22,10 +22,16 @@ def dataReg():
 
 def textReg():
 	"""Preform regression on text data."""
-	xTrain = db.load('train', 'text')[:, 1:].astype(int)
-	yTrain = np.squeeze(db.load('train', 'y')[:, 1:])
-	xValid = db.load('valid', 'text')[:, 1:].astype(int)
-	yValid = np.squeeze(db.load('valid', 'y')[:, 1:])
+	xTrain = db.load('train', 'text').astype(int)
+	yTrain = db.load('train', 'y')
+	xValid = db.load('valid', 'text').astype(int)
+	yValid = db.load('valid', 'y')
+	iTrain = np.isin(yTrain[:, 0], xTrain[:, 0])
+	xTrain = xTrain[:, 1:]
+	yTrain = yTrain[iTrain, 1]
+	iValid = np.isin(yValid[:, 0], xValid[:, 0])
+	xValid = xValid[:, 1:]
+	yValid = yValid[iValid, 1]
 	model = SGDRegressor(penalty='none', max_iter=100)
 	model.fit(xTrain, yTrain)
 	pred = model.predict(xValid)
