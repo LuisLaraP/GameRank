ee979c751191faf4c6afb34d2c0b4b9455744aee
Luis Lara <luislpatino@gmail.com>
2018-06-01 18:00:30 -0500

    Remove dataReg function

--- gamerank/regression.py
+++ gamerank/regression.py
@@ -9,19 +9,6 @@ from sklearn.linear_model import SGDRegressor
 import gamerank.database as db
 
 
-def dataReg():
-	"""Perform regression on game metadata."""
-	xTrain = db.load('train', 'data')[:, 1:].astype(int)
-	yTrain = np.squeeze(db.load('train', 'y')[:, 1:])
-	xValid = db.load('valid', 'data')[:, 1:].astype(int)
-	yValid = np.squeeze(db.load('valid', 'y')[:, 1:])
-	model = SGDRegressor(penalty='none', max_iter=100)
-	model.fit(xTrain, yTrain)
-	pred = model.predict(xValid)
-	error = np.sqrt(mean_squared_error(yValid, pred))
-	print(error)
-
-
 def imgReg():
 	"""Perform regression on image data."""
 	xTrain = db.load('train', 'img').astype(int)
