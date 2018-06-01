f49dd0be1f990ad052689cde921133602ce241f2
Luis Lara <luislpatino@gmail.com>
2018-05-31 20:02:03 -0500

    Perform regression on image data

--- gamerank/regression.py
+++ gamerank/regression.py
@@ -22,11 +22,25 @@ def dataReg():
 
 def imgReg():
 	"""Perform regression on image data."""
-	pass
+	xTrain = db.load('train', 'img').astype(int)
+	yTrain = db.load('train', 'y')
+	xValid = db.load('valid', 'img').astype(int)
+	yValid = db.load('valid', 'y')
+	iTrain = np.isin(yTrain[:, 0], xTrain[:, 0])
+	xTrain = xTrain[:, 1:]
+	yTrain = yTrain[iTrain, 1]
+	iValid = np.isin(yValid[:, 0], xValid[:, 0])
+	xValid = xValid[:, 1:]
+	yValid = yValid[iValid, 1]
+	model = SGDRegressor(penalty='none', max_iter=100)
+	model.fit(xTrain, yTrain)
+	pred = model.predict(xValid)
+	error = np.sqrt(mean_squared_error(yValid, pred))
+	print(error)
 
 
 def textReg():
-	"""Preform regression on text data."""
+	"""Perform regression on text data."""
 	xTrain = db.load('train', 'text').astype(int)
 	yTrain = db.load('train', 'y')
 	xValid = db.load('valid', 'text').astype(int)
